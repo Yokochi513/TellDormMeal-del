06a6c460ff9d06ed56476784113115342602bea3
yokochi <yokochi5123@gmail.com>
2024-06-03 12:36:38 +0900

    Fix/make_json-Func

--- app/TellDormMeal.py
+++ app/TellDormMeal.py
@@ -2,7 +2,11 @@ import datetime
 import requests
 import pdfplumber
 import json
+import os
 from dateutil.relativedelta import relativedelta
+
+PDF_NAME = "ryoumenu.pdf"
+JSON_NAME = "ryoumenu.json"
 
 def get_MealData():
     date = datetime.date.today()
@@ -15,7 +19,7 @@ def get_MealData():
     res = requests.get(pdf_url, allow_redirects=True, verify=False)
 
     if res.status_code == 200:
-        open("ryoumenu.pdf", "wb").write(res.content)
+        open(PDF_NAME, "wb").write(res.content)
         print("HTTP_SUCCESS")
         return True
     else:
@@ -24,7 +28,7 @@ def get_MealData():
 
 
 def analysis_pdf():
-    with pdfplumber.open("ryoumenu.pdf") as pdf:
+    with pdfplumber.open(PDF_NAME) as pdf:
         for page in pdf.pages:
             tables = page.extract_tables()
             dates = tables[0][0]
@@ -37,59 +41,61 @@ def analysis_pdf():
 
 
 def make_json():
-    try:
-        f = open("ryoumenu.json","r")
-        dict_json = json.load(f)
-        f.close()
-
-        for i in range(7):
-            dates,breakfast,lunchA,lunchB,dinnerA,dinnerB =  analysis_pdf()
-
-            dict_json["food"][i]["date"] = dates[i+2]
-
-            if isinstance(breakfast[i+2], str):
-                dict_json["food"][i]["breakfast"] = breakfast[i+2]
-            else:
-                dict_json["food"][i]["breakfast"] = ""
-
-            if isinstance(lunchA[i+2], str):
-                dict_json["food"][i]["lunchA"] = lunchA[i+2]
-            else:
-                dict_json["food"][i]["lunchA"] = ""
-            
-            if isinstance(lunchB[i+2], str):
-                dict_json["food"][i]["lunchB"] = lunchB[i+2]
-            else:
-                dict_json["food"][i]["lunchB"] = ""
-
-            if isinstance(dinnerA[i+2], str):
-                dict_json["food"][i]["dinnerA"] = dinnerA[i+2]
-            else:
-                dict_json["food"][i]["dinnerA"] = ""
-
-            if isinstance(lunchB[i+2], str):
-                dict_json["food"][i]["dinnerB"] = dinnerB[i+2]
-            else:
-                dict_json["food"][i]["dinnerB"] = ""
-
-        new_json = open("ryoumenu.json", "w")
-        json.dump(dict_json,new_json,indent=4)
-        return True
+    dataList = []
     
-    except:
-        return False
+
+    dates,breakfast,lunchA,lunchB,dinnerA,dinnerB =  analysis_pdf()
+
+    for i in range(7):
+        data = {
+            "date" : str,
+            "breakfast" : str,
+            "lunchA" : str,
+            "lunchB" : str,
+            "dinnerA" : str,
+            "dinnerB" : str
+        }
+        data["date"] = dates[i+2]
+        if isinstance(breakfast[i+2], str):
+            data["breakfast"] = breakfast[i+2]
+        else:
+            data["breakfast"] = ""
+        
+        if isinstance(lunchA[i+2], str):
+            data["lunchA"] = lunchA[i+2]
+        else:
+            data["lunchA"] = ""
+        
+        if isinstance(lunchB[i+2], str):
+            data["lunchB"] = lunchB[i+2]
+        else:
+            data["lunchB"] = ""
+        
+        if isinstance(dinnerA[i+2], str):
+            data["dinnerA"] = dinnerA[i+2]
+        else:
+            data["dinnerA"] = ""
+        
+        if isinstance(dinnerB[i+2], str):
+            data["dinnerB"] = dinnerB[i+2]
+        else:
+            data["dinnerB"] = ""
+        dataList.append(data)
+    with open(JSON_NAME, "w") as json_file:
+        json.dump(dataList, json_file, indent=4)
+    os.remove(PDF_NAME)
 
 
 def read_json(weekday):
-    f = open("ryoumenu.json", "r")
+    f = open(JSON_NAME, "r")
     now_json = json.load(f)
     f.close()
-    date = now_json["food"][weekday]["date"]
-    breakfast = now_json["food"][weekday]["breakfast"]
-    lunchA = now_json["food"][weekday]["lunchA"]
-    lunchB = now_json["food"][weekday]["lunchB"]
-    dinnerA = now_json["food"][weekday]["dinnerA"]
-    dinnerB = now_json["food"][weekday]["dinnerB"]
+    date = now_json[weekday]["date"]
+    breakfast = now_json[weekday]["breakfast"]
+    lunchA = now_json[weekday]["lunchA"]
+    lunchB = now_json[weekday]["lunchB"]
+    dinnerA = now_json[weekday]["dinnerA"]
+    dinnerB = now_json[weekday]["dinnerB"]
 
     return date,breakfast,lunchA,lunchB,dinnerA,dinnerB
 
@@ -107,7 +113,7 @@ def notice_update():
 
 
 def json_already_update():
-    f = open("ryoumenu.json", "r")
+    f = open(JSON_NAME, "r")
     now_json = json.load(f)
     f.close()
 
@@ -115,7 +121,7 @@ def json_already_update():
     thisWeekMonDate = datetime.date.today() + relativedelta(days=-weekday)
     thisWeekMon = thisWeekMonDate.strftime("%Y/%m%d").split("/")[1]
     
-    MonOrigin = now_json["food"][0]["date"].split("月")
+    MonOrigin = now_json[0]["date"].split("月")
     month = MonOrigin[0]
     date = MonOrigin[1].split("日")[0]
     if len(month) == 1:
